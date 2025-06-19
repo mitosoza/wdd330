1dec34e0f795ca80d1aea9a7c5786191b27dd959
mitosoza <msozagonzalez@byupathway.edu>
2025-06-19 10:52:12 -0700

    Final Project - Week07

--- scripts/get-hero-images.js
+++ scripts/get-hero-images.js
@@ -1,18 +1,18 @@
 const accessKey = '-fZrsDUc22ovCEmnXpy39NpYRfO5K_6XGD9U6BJvd6M';
 
 export async function getHeroImageUrl() {
-  // const img = document.getElementById('hero-image');
-  // if (!img) return;
-  // try {
-  //   const response = await fetch(
-  //     `https://api.unsplash.com/photos/random?orientation=landscape&query=car&client_id=${accessKey}`
-  //   );
-  //   const data = await response.json();
-  //   img.src = data.urls.regular;
-  //   img.alt = data.alt_description || 'Car image';
-  // } catch (e) {
-  //   img.alt = 'Failed to load car image';
-  // }
+  const img = document.getElementById('hero-image');
+  if (!img) return;
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?orientation=landscape&query=car&client_id=${accessKey}`
+    );
+    const data = await response.json();
+    img.src = data.urls.regular;
+    img.alt = data.alt_description || 'Car image';
+  } catch (e) {
+    img.alt = 'Failed to load car image';
+  }
 }
 
 getHeroImageUrl();
